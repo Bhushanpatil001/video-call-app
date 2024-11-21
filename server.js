baf2c5cdcf2a1642bab0d1181f805c33ddfcf491
Bhushanpatil001 <bhushanpatil0117@gmail.com>
2024-11-21 17:38:09 +0530

    Changed server and client files , for Network interruption and roomFull

--- server.js
+++ server.js
@@ -3,77 +3,52 @@ const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
-
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(cors());
-const rooms = {};
-const users = {};
+const rooms = {}; // Object to hold rooms and users
+const users = {}; // Map to hold socket ids and usernames
 
 // Serve static files
 app.use(express.static('public'));
 
-// Socket.io
+// Socket.io connection
 io.on('connection', (socket) => {
-    // console.log(`New connection: ${socket.id}`);
+    console.log(`New connection: ${socket.id}`);
 
     // Register User
     socket.on('registerUser', (username) => {
         users[socket.id] = username;
-        // console.log(`${username} registered with ID: ${socket.id}`);
-        // console.log({users});
+        console.log(`${username} registered with ID: ${socket.id}`);
         socket.emit('userRegistered', socket.id);
     });
 
     // Join Room
-    // socket.on('joinRoom', (roomId, userId) => {
-    //     console.log("Room Id : " + roomId + "userID" + userId);
-    //     if (!rooms[roomId]) {
-    //         rooms[roomId] = [];
-    //     }
-    //     rooms[roomId].push(userId);
-    //     socket.join(roomId);
-    //     console.log(`${userId} joined room ${roomId}`);
-    // });
-
-
-    // Join Room
-    // socket.on('joinRoom', (roomId) => {
-    //     // Use socket.id as the userId
-    //     const userId = socket.id;
-    //     console.log(`${userId} joined room ${roomId}`);
-
-    //     if (!rooms[roomId]) {
-    //         rooms[roomId] = [];
-    //     }
-    //     rooms[roomId].push(userId);
-    //     socket.join(roomId);  // Join the room
-    //     console.log("users", users);
-    //     console.log("rooms", rooms);
-
-    //     // You can optionally emit back to the client the list of users in the room if needed
-    //     socket.emit('joinedRoom', roomId, userId);  // Inform the client they've joined
-    // });
-
-
     socket.on('joinRoom', (roomId) => {
-        if (!rooms[roomId]) rooms[roomId] = [];
-        
-        if(rooms[roomId].length === 2){
-            console.log('Room is full. Cannot join');
-            socket.emit("roomFull", roomId);
-            return;
-        }else{
-            if (!rooms[roomId].includes(socket.id)) rooms[roomId].push(socket.id);
+        if (!rooms[roomId]) {
+            rooms[roomId] = []; // Initialize room if it doesn't exist
         }
+
+        if (rooms[roomId].length === 2) {
+            // If the room already has 2 users, it is full
+            console.log('Room is full');
+            socket.emit('roomFull', roomId); // Emit room full error to client
+            return;
+        }
+
+        // Add the user to the room
+        rooms[roomId].push(socket.id);
         socket.join(roomId);
-        // console.log(`${socket.id} rejoined room ${roomId}`);
-        console.log('users in room', roomId, rooms[roomId]);
+        console.log(`${socket.id} joined room ${roomId}`);
+
+        // Notify the client that they have successfully joined
+        socket.emit('joinedRoom', roomId);
+        io.to(roomId).emit('userJoined', socket.id); // Notify others in the room
     });
 
-    // Handle WebRTC Signaling
+    // WebRTC Signaling
     socket.on('offer', (roomId, userId, offer) => {
         socket.to(roomId).emit('offerReceived', userId, offer);
     });
@@ -86,15 +61,17 @@ io.on('connection', (socket) => {
         socket.to(roomId).emit('iceCandidate', userId, candidate);
     });
 
-    // Disconnect
+    // Disconnect user from the room when they leave
     socket.on('disconnect', () => {
-    // console.log(`${socket.id} disconnected`);
-    // console.log('users in room', roomId, rooms[roomId]);
-    // console.log(rooms);
+        // Remove the user from all rooms
         Object.keys(rooms).forEach((roomId) => {
             rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+            if (rooms[roomId].length === 0) {
+                delete rooms[roomId]; // Clean up empty rooms
+            }
         });
-    delete users[socket.id];
+        delete users[socket.id]; // Remove user from the users map
+        console.log(`${socket.id} disconnected`);
     });
 });
 
